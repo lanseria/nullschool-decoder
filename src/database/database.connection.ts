5de78102013638f8dd8ec71946172edf4e1b9a93
lanseria <zhangchao564265135@hotmail.com>
2022-02-11 10:22:46 +0800

    remove unusefull code

--- src/database/database.connection.ts
+++
@@ -1,5 +0,0 @@
-import { createConnection, Connection, ConnectionOptions } from "typeorm";
-const connectionOpts: ConnectionOptions = require("./database.connection.options");
-const connection: Promise<Connection> = createConnection(connectionOpts);
-
-export default connection;
